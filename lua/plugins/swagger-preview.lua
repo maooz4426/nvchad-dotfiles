12e6285bc9a702ef664f6f453d7bbefeff091196
MAOZ <maooz4426@gmail.com>
2026-01-25 02:43:06 +0900

    add swagger-preview

--- lua/plugins/swagger-preview.lua
+++ lua/plugins/swagger-preview.lua
@@ -2,5 +2,10 @@ return {
   "vinnymeller/swagger-preview.nvim",
   cmd = { "SwaggerPreview", "SwaggerPreviewStop", "SwaggerPreviewToggle" },
   build = "npm i",
-  config = true,
+  config = function()
+    require("swagger-preview").setup({
+      port = 8003,
+      host = "localhost",
+    })
+  end
 }
